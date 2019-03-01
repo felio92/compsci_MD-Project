60f6cdf2583d01d303f6127cec3b4322bd4cbaa9
Felix Oertel <felix_oertel402@hotmail.com>
2019-03-01 22:30:28 +0100

    Made some changes to optimize and added (faulty) descent example to Ewald

--- optimize.py
+++ optimize.py
@@ -1,34 +1,7 @@
 import numpy as np
-import torch
-import random
-
-from potentials import *
 from distances import *
-from sampling import *
 
-def dist_torch(x): 
-    """Calculates distance vectors and distances (euclidian norm of vecs)
-    
-    Arguments:
-        x (float): position vectors (dim = N x 3)
-    
-    Output:
-        dist (float): distances between particle pairs (dim = N x N)
-        vecs (float): distance vectors between particle pairs (dim = N x N x 3)
-    """
-    x = torch.Tensor(x)
-    vecs = x[None, :, :] - x[:, None, :]       
-    return torch.norm(vecs, dim=-1), vecs
-
-def gradLJ_t(x, sig=1, eps=1):
-    dist, vecs = dist_torch(x)
-    dist[dist!=0] = 1/dist[dist!=0]
-    D_att = 6 * sig**6 * dist**8
-    D_rep = -12 * sig**12 * dist**14
-    D = 4*(eps*(D_att + D_rep))[:, :, None]*vecs
-    return torch.sum(D, dim=-2)
-
-def descent( x, q, grad, a=1e-4, prec=1e-10, maxst=1e6, k=.1, boxsize=(0, 1) ):
+def descent(x, grad, a=1e-4, prec=1e-10, maxst=1e5, boxsize=(0, 1), pbc=False, ewald=False ):
     """Gradient Descent
     
     Arguments:
@@ -44,14 +17,18 @@ def descent( x, q, grad, a=1e-4, prec=1e-10, maxst=1e6, k=.1, boxsize=(0, 1) ):
         step: # of steps needed to convergence
     """
     x = x[None, :, :]
+    x_min, x_max = boxsize[0], boxsize[1]
     step = 0
-    vecs = vectors(x[-1], boxsize)
-    f = grad(x[-1], q)
+    vecs = vectors(x[-1], boxsize, pbc=pbc^ewald)
+    f = grad(x[-1])
     x1 = x[-1] - a * f
     while step < maxst and np.linalg.norm(x[-1] - x1) > prec:
+        if pbc:
+            x1 = x1 - (x1 > x_max)*(x_max-x_min)
+            x1 = x1 + (x1 < x_min)*(x_max-x_min)
         x = np.append(x, x1[None, :, :], axis=0)
-        vecs = vectors(x[-1], boxsize)
-        f = grad(x[-1], q)
+        vecs = vectors(x[-1], boxsize, pbc=pbc^ewald)
+        f = grad(x[-1])
         x1 = x[-1] - a * f
         step += 1
     return x, step
