ee83fa6069f0b1792a7b363650ef5c88dbe37318
masc <fu2013ni@zedat.fu-berlin.de>
2019-03-03 17:20:06 +0100

    bugfix, save_config

--- optimize.py
+++ optimize.py
@@ -1,39 +1,44 @@
 import numpy as np
-from distances import *
 
-def descent(x, grad, a=1e-4, prec=1e-10, maxst=1e5, boxsize=(0, 1), pbc=False, save_config=True, ewald=False ):
+def descent( x, grad, a=1e-4, prec=1e-6, maxst=1e5, boxsize=(0,1), pbc=False, save_config=False):
     """Gradient Descent
-    
+
     Arguments:
-        x    (float): position vectors (dim = n x 3)
-        q: charge
-        a    (float): 'learning rate' alpha = 1e-4
-        prec (float): difference between steps, precision = 1e-10
-        maxst  (int): max # of steps, maxst = 1e6
-        k: factor harmonic pot
-    
+        coord    (float): position vectors (dim = n x 3)
+        grad  (function): gradient calculation
+        a        (float): alpha, 'learning rate', influence of gradient per step
+        prec     (float): precision, difference between steps
+        maxst      (int): max # of steps
+        boxsize (double): tuple, box is quadratic/cubic
+        pbc == True: enables periodic boundaries
+        save_config == True: saves all positions
+
     Output:
-        x: position array,
-        step: # of steps needed to convergence
-    """
-    if save_config:
-        config = np.zeros((maxst,)+x.shape)
-        config[0] = x
-    x_min, x_max = boxsize[0], boxsize[1]
+        x    (float): tensor of position vectors at each step (dim = n x 3 x step)
+        step: # of steps needed to converge"""
+    assert int(maxst) % maxst == 0
+    # Initialize:
     step = 0
-    vecs = vectors(x, boxsize, pbc=pbc^ewald)
-    f = grad(x)
-    x1 = x - a * f
-    step+=1
-    while step < maxst and np.linalg.norm(x - x1) > prec:
-        if pbc:
-            x1 = x1 - (x1 > x_max)*(x_max-x_min)
-            x1 = x1 + (x1 < x_min)*(x_max-x_min)
-        if save_config: config[step] = x1
-        vecs = vectors(x1, boxsize, pbc=pbc^ewald)
-        f = grad(x1)
-        x1 = x1 - a * f
-        step += 1
     if save_config:
-        return config[:step], step
-    return x1, step
+        config = np.zeros((int(maxst),) + x.shape)
+        config[step] = x
+    x1 = x - a * grad(x)
+    if pbc:
+        xmin, xmax = boxsize[0], boxsize[1]
+        assert xmin < xmax
+        L = xmax - xmin
+        x1[x1 < xmin] = xmax - (xmin - x1[x1 < xmin]) %  L
+        x1[x1 > xmax] = xmin + (x1[x1 > xmax] - xmax) %  L
+    # Calculate gradient until convergence:
+    while step+1 < maxst and np.linalg.norm(x - x1) > prec:
+        step += 1
+        if save_config: config[step] = x1
+        x = x1
+        x1 = x - a * grad(x)
+        if pbc:
+            x1[x1 < xmin] = xmax - (xmin - x1[x1 < xmin]) %  L
+            x1[x1 > xmax] = xmin + (x1[x1 > xmax] - xmax) %  L
+    if save_config:
+        return config[:step+1], step
+    return x1
+
